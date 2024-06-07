d3b9911c64c44fd436a051c567d9dc7d8a24bad1
David Lynch <kemayo@gmail.com>
2024-06-07 14:38:53 -0500

    Add settings, using LaunchAtLogin package

--- LoosePhabric/LoosePhabricApp.swift
+++ LoosePhabric/LoosePhabricApp.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import LaunchAtLogin
 
 @main
 struct LoosePhabricApp: App {
@@ -15,15 +16,39 @@ struct LoosePhabricApp: App {
         MenuBarExtra("T", systemImage: "tray.and.arrow.down") {
             AppMenu()
         }
+        Settings {
+            SettingsView()
+        }
     }
 }
 
 struct AppMenu: View {
     var body: some View {
         Label("LoosePhabric", systemImage: "book")
+        if #available(macOS 14.0, *) {
+            SettingsLink()
+        } else {
+            Button("Settings...") {
+                if #available(macOS 13.0, *) {
+                    NSApp.sendAction(Selector(("showSettingsWindow:")), to: nil, from: nil)
+                } else {
+                    NSApp.sendAction(Selector(("showPreferencesWindow:")), to: nil, from: nil)
+                }
+            }
+        }
         Divider()
         Button("Quit") {
             NSApplication.shared.terminate(nil)
         }.keyboardShortcut("q")
+    }
+}
+
+struct SettingsView: View {
+    var body: some View {
+        Form {
+            LaunchAtLogin.Toggle()
+        }
+        .padding(20)
+        .frame(width: 350, height: 100)
     }
 }
