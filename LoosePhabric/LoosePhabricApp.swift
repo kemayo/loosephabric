3253941999905c74c334fea275a76e2ad1f48c56
David Lynch <kemayo@gmail.com>
2024-06-07 09:30:40 -0500

    Initial Commit

---
+++ LoosePhabric/LoosePhabricApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoosePhabricApp.swift
+//  LoosePhabric
+//
+//  Created by David Lynch on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct LoosePhabricApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LoosePhabricUITests/LoosePhabricUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  LoosePhabricUITestsLaunchTests.swift
+//  LoosePhabricUITests
+//
+//  Created by David Lynch on 6/7/24.
+//
+
+import XCTest
+
+final class LoosePhabricUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
