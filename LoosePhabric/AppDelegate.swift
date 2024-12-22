kemayo/loosephabric

commit 65f4986d44ea46f207a43bde06e24e9f060bcc1f

Remove the guards that made double-copying something suppress a fetch
Paste-without-formatting will work for accessing the original copied
text, so this was unnecessarily confusing.

Fixes #9

LoosePhabric/AppDelegate.swift
@@ -12,9 +12,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     let pasteboard: NSPasteboard = .general
     var lastChangeCount: Int = 0
 
-    var lastInputValue: String?
-    var lastSetValue: String?
-
     let nc = NotificationCenter.default
     let publisher = NotificationCenter.default.publisher(for: Notification.Name("NSPasteboardDidChange"))
 
@@ -36,10 +33,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         guard let items = pasteboard.pasteboardItems else { return }
         guard let item = items.first else { return }
         guard let plain = item.string(forType: .string) else { return }
-
-        if plain == lastInputValue || plain == lastSetValue { return }
-        lastSetValue = nil
-        lastInputValue = plain
 
         if fetchPhabricatorTitleAndSetLink(text: plain) { return }
         if fetchGerritTitleAndSetLink(text: plain) { return }
@@ -209,7 +202,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         pasteboard.setString(original, forType: .string)
         // For completeness:
         pasteboard.setString(url, forType: .URL)
-        lastSetValue = text
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {

commit 954dd8f6ef9ff241d012aae3cf749509cc9371a4

Stop our own pasteboard-set from triggering expansion
Guard removal in 65f4986d44ea46f207a43bde06e24e9f060bcc1f caused this.

LoosePhabric/AppDelegate.swift
@@ -23,6 +23,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         ])
         timer = Timer.scheduledTimer(withTimeInterval: 0.05, repeats: true) { (t) in
             if self.lastChangeCount != self.pasteboard.changeCount {
+                print("pasteboard changed", self.lastChangeCount, self.pasteboard.changeCount)
                 self.lastChangeCount = self.pasteboard.changeCount
                 self.onPasteboardChanged()
             }
@@ -202,6 +203,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         pasteboard.setString(original, forType: .string)
         // For completeness:
         pasteboard.setString(url, forType: .URL)
+
+        // Avoid looping
+        self.lastChangeCount += 1
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {